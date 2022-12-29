375c13d6e768a62a48c428420d43e183737a6ab5
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-29 20:10:18 +0900

    Create 1938 아주 간단한 계산기.java

---
+++ SWEA/D1/1938 아주 간단한 계산기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+    b = sc.nextInt();
+        
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+		
+	}
+}
