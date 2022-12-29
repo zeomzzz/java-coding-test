d70f4b0345fb4747ed6b74be05a69c8eee3fc3e6
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-29 20:31:40 +0900

    Create 2043 서랍의 비밀번호.java

---
+++ SWEA/D1/2043 서랍의 비밀번호.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+    b = sc.nextInt();
+        
+    System.out.println(a - b + 1);
+	}
+}
