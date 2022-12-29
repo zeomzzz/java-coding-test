335d11457a9835f6acea487b62596540278cdcad
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-30 00:33:25 +0900

    Create 1936 1대1 가위바위보.java

---
+++ SWEA/D1/1936 1대1 가위바위보.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+  public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+    b = sc.nextInt();
+        
+    if(a - b == 1 || a-b == -2){
+       System.out.println('A');
+    }else{
+       System.out.println('B');
+    }
+
+	}
+}
