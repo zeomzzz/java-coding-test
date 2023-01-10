4e28266b280b0b7d5667273c45e5d76df3f06fa0
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-01-10 21:21:37 +0900

    Update 1936 1대1 가위바위보.java

--- SWEA/D1/1936 1대1 가위바위보.java
+++ SWEA/D1/1936 1대1 가위바위보.java
@@ -5,17 +5,15 @@ class Solution
 {
   public static void main(String args[]) throws Exception
 	{
-
-		Scanner sc = new Scanner(System.in);
-		int a, b;
-		a = sc.nextInt();
-    b = sc.nextInt();
-        
-    if(a - b == 1 || a-b == -2){
-       System.out.println('A');
-    }else{
-       System.out.println('B');
-    }
-
+	  Scanner sc = new Scanner(System.in);
+	  int a, b;
+	  a = sc.nextInt();
+	  b = sc.nextInt();
+	  
+	  if(a - b == 1 || a-b == -2){
+		  System.out.println('A');
+	  }else{
+		  System.out.println('B');
+	  }
 	}
 }
