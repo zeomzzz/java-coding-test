4c29916cba16df8aa70030cb87b0a3cf552ad805
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-01-13 08:50:30 +0900

    Create 18108.java

---
+++ BOJ/Bronze/18108.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		int ans = y - (2541 - 1998);
+		
+		System.out.println(ans);
+
+	}
+
+}
