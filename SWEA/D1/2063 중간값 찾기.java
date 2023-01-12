4e1fa69d3b83c6f683e6424a7bceb8d4be2df6c5
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-01-12 09:02:53 +0900

    Create 2063 중간값 찾기.java

---
+++ SWEA/D1/2063 중간값 찾기.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		} // N개 입력받음
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[(N - 1) / 2]);
+		
+	} // scanner
+}
