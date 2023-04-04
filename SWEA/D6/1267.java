efaa45561796b0d2324240c787e8975f7d012661
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-04-04 11:33:47 +0900

    Update 1267.java

--- SWEA/D6/1267.java
+++ SWEA/D6/1267.java
@@ -68,3 +68,68 @@ public class Solution {
 		}
 	}
 }
+
+// 2. Stack(재귀)
+// 주의 : 비트마스킹 안되는 문제 (범위 이슈)
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+	
+	static boolean[] visited;
+	static Stack<Integer> stack;
+	static ArrayList<Integer>[] adj;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int tc=1; tc<=10; tc++) {
+			
+			int v = sc.nextInt(); // 정점의 개수
+			int e = sc.nextInt(); // 간선의 개수
+			
+			adj = new ArrayList[v+1];
+			for(int i=0; i<v+1; i++) adj[i] = new ArrayList<>(); // 유향그래프 입력 
+			int[] indegree = new int[v+1]; // 진입차수 입력
+			
+			for(int i=0; i<e; i++) {
+				int start = sc.nextInt();
+				int end = sc.nextInt();
+				adj[start].add(end);
+				indegree[end]++;
+			}
+			
+			stack = new Stack<>(); // 정답 담을 stack
+			visited = new boolean[v+1];
+			for(int i=1; i<v+1; i++) {
+				if(indegree[i] == 0) tpg(i); // 선행조건 없는 정점에서 출발
+			}
+			
+			// 정답 출력할 sb
+			StringBuilder sb = new StringBuilder();
+			sb.append("#" + tc);
+			while(!stack.isEmpty()) sb.append(" " + stack.pop());
+			
+			System.out.println(sb);
+			
+		} // tc 10개
+	}
+
+	private static void tpg(int cur) {
+		visited[cur] = true; // 방문처리
+		
+		for(int next : adj[cur]) { // cur 다음에 갈 수 있는 정점 중에
+			if(visited[next] == false) { // 방문 안했으면
+				tpg(next); // dfs ㄱㄱ
+			}
+		}
+		
+		stack.add(cur);
+		
+	}
+
+}
+
