92d9917983abc6da716d49af48e5d41861e1855f
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-04-10 15:35:35 +0900

    Update 5215.java

--- SWEA/D3/5215.java
+++ SWEA/D3/5215.java
@@ -58,3 +58,44 @@ public class Solution {
          
     }
 }
+
+
+// DP 풀이
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt(); // tc개수
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			int N = sc.nextInt(); // 재료의 수
+			int L = sc.nextInt(); // 제한 칼로리
+
+			int[] scores = new int[N+1];
+			int[] cals = new int[N+1];
+			for (int i = 1; i <= N; i++) {
+				scores[i] = sc.nextInt(); // 맛에 대한 점수
+				cals[i] = sc.nextInt(); // 칼로리
+			}
+
+			int[][] dp = new int[N+1][L+1];
+			for (int i = 1; i <= N; i++) { // 아이템 0 ~ N-1 로 N가지
+				for (int l = 0; l <= L; l++) {
+					if (cals[i] <= l) { // 칼로리가 범위 안
+						dp[i][l] = Math.max(dp[i-1][l], dp[i-1][l-cals[i]] + scores[i]);
+					} else {
+						dp[i][l] = dp[i-1][l];
+					}
+				}
+			}
+			
+			System.out.printf("#%d %d\n", tc, dp[N][L]);
+
+		} // tc
+	} // main
+
+}
