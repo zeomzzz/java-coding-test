e7a460210121bedaed0bb40d52b4c4f61a9c907e
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-10-11 20:22:37 +0900

    Update 12977.java

--- Programmers/Lv.1/12977.java
+++ Programmers/Lv.1/12977.java
@@ -34,3 +34,34 @@ class Solution {
         }
     }
 }
+
+/*
+* 1. 3중 for문으로 3개 고르기
+* 2. 소수인지 확인
+*/
+
+class Solution {
+    
+    public int solution(int[] nums) {
+        int answer = 0;
+        int l = nums.length;
+        
+        for (int i=0; i<l-2; i++) {
+            for (int j=i+1; j<l-1; j++) {
+                for (int k=j+1; k<l; k++) {
+                    if (isPrime(nums[i] + nums[j] + nums[k])) answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    public boolean isPrime(int sum) {
+        for (int i=2; i*i<=sum; i++) {
+            if(sum % i == 0) return false;
+        }
+        return true;
+    }
+    
+}
