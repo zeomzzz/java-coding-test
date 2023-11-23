79d5a12dfaa67573e5581e1bf27644f3644bd4bf
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-11-23 14:34:55 +0900

    Create 12914.java

---
+++ Programmers/Lv.2/12914.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int n) {
+        
+        int[] dp = new int[n + 1];
+        if (n >= 1) {
+            dp[1] = 1;
+        }
+        if (n >= 2) {
+            dp[2] = 2;
+        }
+        if (n >= 3) {
+            for (int i=3; i<= n; i++) {
+                dp[i] = dp[i-2] + dp[i-1];
+                dp[i] %= 1234567;
+            }
+        }
+             
+        return dp[n];
+    }
+}
