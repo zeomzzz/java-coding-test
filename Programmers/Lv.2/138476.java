b8983021ca70d02fa4296f57c7482d3821361313
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-11-26 22:14:29 +0900

    Update 138476.java

--- Programmers/Lv.2/138476.java
+++ Programmers/Lv.2/138476.java
@@ -38,3 +38,36 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이 : getOrDefault 이용
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        if (k == 1) {
+            return 1;
+        }
+        
+        Map<Integer, Integer> sizes = new HashMap<>();
+        
+        for (int size : tangerine) {
+            sizes.put(size, sizes.getOrDefault(size, 0) + 1);
+        }
+        
+        List<Integer> keys = new ArrayList<>(sizes.keySet());
+        Collections.sort(keys, (v1, v2) -> (sizes.get(v2).compareTo(sizes.get(v1))));
+        
+        int limit = k;
+        for (int size : keys) {
+            answer++;
+            limit -= sizes.get(size);
+            if (limit <= 0) {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
