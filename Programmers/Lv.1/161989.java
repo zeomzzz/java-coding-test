269b6fa33c421eeb8d291e32255da2477edcfba6
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-12-03 00:12:57 +0900

    Update 161989.java

--- Programmers/Lv.1/161989.java
+++ Programmers/Lv.1/161989.java
@@ -1,16 +1,17 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        
-        int size = section.length;
-        int nxt = section[0];
+        int answer = 1;
         int idx = 0;
+        int l = section.length;
+        int start = section[idx];
+        int end = start + m - 1;
         
-        while(idx < size) {
-            if (nxt <= section[idx]) {
-                nxt = section[idx] + m;
+        while (end < n+1 && idx < l) {
+            if (section[idx] > end) {
+                start = section[idx];
+                end = start + m - 1;
                 answer++;
-            }
+            } 
             idx++;
         }
         
