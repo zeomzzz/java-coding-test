4bab7ff51d5439513d1fdfb77f1f16f948850bf9
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-12-10 14:51:21 +0900

    Create 42587.java

---
+++ Programmers/Lv.2/42587.java
@@ -0,0 +1,50 @@
+// 1. priorites는 idx와 함께 queue에 담고, pq를 하나 더 만들기
+// 2. pq를 pop하면서 일치하면 실행(answer++). 아니면 다시 queue에 넣기
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<int[]> prioritiesQ = new LinkedList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int l = priorities.length;
+        
+        // 1. queue에 담기
+        for (int i=0; i<l; i++) {
+            int idx = i;
+            int priority = priorities[i];
+            int[] element = new int[2];
+            element[0] = priority;
+            element[1] = idx;
+            
+            prioritiesQ.offer(element);
+            pq.offer(priority);
+        }
+        
+        // 2. pq를 pop하면서 실행
+        outer : while (pq.size() > 0) {
+            // 실행해야하는 우선순위
+            int priority = pq.poll();
+            
+            while (prioritiesQ.size() > 0) {
+                int[] polled = prioritiesQ.poll();
+                
+                // 실행하는 경우
+                if (polled[0] == priority) {
+                    answer++;
+                    // location을 실행하는 경우
+                    if (polled[1] == location) {
+                        break outer;
+                    }
+                    break;
+                // 실행하지 않는 경우 다시 queue에 넣기
+                } else {
+                    prioritiesQ.offer(polled);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
