99e63b09c35a12653ca02b9ba7badbd6ef84433a
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-12-16 01:40:59 +0900

    Update 42577.java

--- Programmers/Lv.2/42577.java
+++ Programmers/Lv.2/42577.java
@@ -1,6 +1,6 @@
-// 1. 길이 순으로 정렬 -> 안해도 될듯
+// 1. 길이 순으로 정렬 -> 안해도 될듯 -> 사전순으로 정렬!!
 // 2. 완탐
-// 실행결과 : 정확성 20/20, 효율성 2/4 -> 어떻게 효율성 높일 것인가..
+// 실행결과 : 정확성 20/20, 효율성 2/4 -> 어떻게 효율성 높일 것인가.. -> String을 사전순으로 정렬하면?
 
 import java.util.*;
 
@@ -39,6 +39,40 @@ class Solution {
                             }
                         }
                     }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
+
+// 효율성 해결
+// 1. 사전 순으로 정렬
+// 2. 다음 것과만 비교
+
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        int l = phone_book.length;
+        
+        Arrays.sort(phone_book);
+        
+        for (int i=0; i<l-1; i++) {
+            int n = phone_book[i].length();
+            int m = phone_book[i+1].length();
+            
+            if (n < m) {
+                if (phone_book[i].equals(phone_book[i+1].substring(0, n))) {
+                    answer = false;
+                    break;
+                }
+            } else if (n > m) {
+                if (phone_book[i+1].equals(phone_book[i].substring(0, m))) {
+                    answer = false;
+                    break;
                 }
             }
         }
