f3a68b96ec705abde4bd9a676b5fbf8e9765c69c
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-12-20 14:06:01 +0900

    Update 2068 최대수 구하기.java

--- SWEA/D1/2068 최대수 구하기.java
+++ SWEA/D1/2068 최대수 구하기.java
@@ -59,3 +59,37 @@ public class Solution {
 		
 	}
 }
+
+// 풀이 3 : BufferedReader + 버블정렬
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        int[] arr = new int[10];
+
+        for (int tc=1; tc<=t; tc++) {
+            st = new StringTokenizer(br.readLine());
+            // 배열 세팅
+            for (int i=0; i<10; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            // 버블정렬 1회 진행하면 맨 끝에 가장 큰 수
+            for (int i=0; i<9; i++) {
+                if (arr[i] > arr[i+1]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = tmp;
+                    tmp = arr[i];
+                }
+            }
+
+            System.out.printf("#%d %d\n", tc, arr[9]);
+        }
+        
+    }
+}
