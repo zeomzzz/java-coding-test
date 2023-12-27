58634d51e6c688b0d116fad176682b386f3d0b38
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-12-27 23:24:08 +0900

    Update 2309.java

--- BOJ/Bronze/2309.java
+++ BOJ/Bronze/2309.java
@@ -37,3 +37,44 @@ public class Main {
     }
 
 }
+
+// 2중 for문 풀이 : 제외할 난쟁이를 찾기 (76ms)
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int[] hs = new int[9];
+    	int sum = 0;
+    	for (int i=0; i<9; i++) {
+    		hs[i] = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+    		sum += hs[i];
+    	}
+    	
+    	Arrays.sort(hs);
+    	
+    	boolean flag = true;
+    	
+    	// 제외할 두 난쟁이를 찾기
+    	outer: for (int i=0; i<8; i++) {
+    		for (int j=i+1; j<9; j++) {
+    			if (sum - (hs[i] + hs[j]) == 100) {
+    				for (int idx=0; idx<9; idx++) {
+    					if (idx!=i && idx!=j) {
+    						System.out.println(hs[idx]);
+        					flag = true;
+    					}
+    				}
+    				if (flag) {
+    					break outer;
+    				}
+    			}
+    		}
+    	}
+    	
+    }
+
+}
