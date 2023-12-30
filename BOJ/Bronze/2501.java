1e533995144f362124583031ba75f2e2897e35ad
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-12-30 13:28:47 +0900

    Create 2501.java

---
+++ BOJ/Bronze/2501.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int N = Integer.parseInt(st.nextToken());
+    	int K = Integer.parseInt(st.nextToken());
+    	
+    	int cnt = 0;
+    	int ans = 0;
+    	for (int i=1; i<=N; i++) {
+    		if (N % i == 0) {
+    			cnt++;
+    		}
+    		if (cnt == K) {
+    			ans = i;
+    			break;
+    		}
+    	}
+    	
+    	System.out.println(ans);
+    	
+    }
+    
+};
