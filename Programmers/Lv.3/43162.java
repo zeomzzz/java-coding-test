de919f5185bf4e66171620769022828f16b279d6
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-12-31 23:31:25 +0900

    Update 43162.java

--- Programmers/Lv.3/43162.java
+++ Programmers/Lv.3/43162.java
@@ -27,3 +27,37 @@ class Solution {
         }
     }
 }
+
+// bfs 풀이
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
+        int[] visited = new int[n];
+        Queue<Integer> q = new LinkedList<>();
+        
+        for (int i=0; i<n; i++) {
+            if (visited[i] == 0) {
+                
+                answer++;
+                q.offer(i);
+                while (!q.isEmpty()) {
+                    
+                    int cur = q.poll();
+                    visited[cur] = 1;
+                    
+                    for (int nxt=0; nxt<n; nxt++) {
+                        if (visited[nxt] == 0 && computers[cur][nxt] == 1) {
+                            q.offer(nxt);
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+        return answer;
+    }
+}
