7d24f421695c3e710a93e2d5e4a540716ddb5c9f
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-01-30 22:30:41 +0900

    Create 15649.java

---
+++ BOJ/Silver/15649.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int N = Integer.parseInt(st.nextToken()); // 1 ~ N
+    	int M = Integer.parseInt(st.nextToken()); // 길이 M
+    	
+    	int[] selected = new int[M];
+    	boolean[] visited = new boolean[N+1];
+    	backtracking(0, N, M, selected, visited);
+    }
+    
+    static void backtracking(int cur, int N, int M, int[] selected, boolean[] visited) {
+    	
+    	if (cur == M) {
+    		StringBuilder sb = new StringBuilder();
+    		for (int select : selected) {
+    			sb.append(select);
+    			sb.append(" ");
+    		}
+    		System.out.println(sb);
+    		return;
+    	}
+    	
+    	for (int i=1; i<=N; i++) {
+    		if (visited[i]) continue;
+    		
+    		selected[cur] = i;
+    		visited[i] = true;
+    		backtracking(cur+1, N, M, selected, visited);
+    		visited[i] = false;
+    	}
+    }
+};
