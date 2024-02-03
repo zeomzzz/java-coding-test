1829120c430afb5428649335d26e82bf95f56155
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-02-03 11:30:43 +0900

    Create 15657.java

---
+++ BOJ/Silver/15657.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+// N개 중 M개를 고름. 중복 가능. 비내림차순. 사전 순 출력
+public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int N = Integer.parseInt(st.nextToken());
+    	int M = Integer.parseInt(st.nextToken());
+    	
+    	int[] nums = new int[N];
+    	st = new StringTokenizer(br.readLine());
+    	for (int i=0; i<N; i++) {
+    		nums[i] = Integer.parseInt(st.nextToken());
+    	}
+    	Arrays.sort(nums);
+    	
+    	int[] selected = new int[M];
+    	backtracking(0, 0, N, M, nums, selected);
+    	
+    	System.out.print(sb);
+    	
+    }
+    
+    static void backtracking(int cur, int start, int N, int M, int[] nums, int[] selected) {
+    	
+    	if (cur == M) {
+    		for (int select : selected) {
+    			sb.append(select);
+    			sb.append(" ");
+    		}
+    		sb.append("\n");
+    		return;
+    	}
+    	
+    	for (int i=start; i<N; i++) {
+    		selected[cur] = nums[i];
+    		backtracking(cur+1, i, N, M, nums, selected);
+    	}
+    	
+    }
+    
+};
