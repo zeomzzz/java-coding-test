b2b60b746da3b3fe6a6a736bf21c0cce441c176b
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-02-05 19:01:05 +0900

    Update 1182.java

--- BOJ/Silver/1182.java
+++ BOJ/Silver/1182.java
@@ -37,3 +37,47 @@ public class Main {
     }
     
 };
+
+// static에 answer 올리기 않는 방법
+import java.io.*;
+import java.util.*;
+
+// 백트래킹으로 확인
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int N = Integer.parseInt(st.nextToken()); // 정수의 개수
+    	long S = Long.parseLong(st.nextToken()); // 합
+    	
+    	st = new StringTokenizer(br.readLine());
+    	long[] nums = new long[N];
+    	for (int i=0; i<N; i++) {
+    		nums[i] = Long.parseLong(st.nextToken());
+    	}
+    	
+    	int answer = 0;
+    	answer += backtracking(0, 0, 0, S, N, nums);
+    	
+    	System.out.print(answer);
+    	
+    }
+    
+    static int backtracking(int cur, int start, long sum, long target, int N, long[] nums) {
+    	
+    	int answer = 0;
+    	
+    	if (cur != 0 && sum == target) {
+    		answer++;
+    	}
+    	
+    	for (int i=start; i<N; i++) {
+    		answer += backtracking(cur+1, i+1, sum+nums[i], target, N, nums);
+    	}
+    	
+    	return answer;
+    }
+    
+};
