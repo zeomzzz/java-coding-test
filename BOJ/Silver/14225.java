66ed9dcc1e9eb98f64a67dca895c65e7d9911a2e
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-02-10 15:57:59 +0900

    Update 14225.java

--- BOJ/Silver/14225.java
+++ BOJ/Silver/14225.java
@@ -47,3 +47,54 @@ public class Main {
     }
     
 };
+
+import java.io.*;
+import java.util.*;
+
+// 백트래킹으로 확인
+// 부분 수열의 합으로 나올 수 없는 가장 작은 자연수
+// 합을 다 오름차순 List에 넣기 : 2**20
+// 순회하면서 가장 작은 값 찾기
+public class Main {
+	
+	static List<Integer> sums = new LinkedList<>();
+	
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int N = Integer.parseInt(br.readLine());
+    	
+    	int[] nums = new int[N];
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	for (int i=0; i<N; i++) {
+    		nums[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	backtracking(0, 0, 0, N, nums);
+    	Collections.sort(sums);
+    	
+    	int min = 1;
+    	for (int sum : sums) {
+    		if (sum < min + 1) {
+    			min = sum + 1;
+    		} else {
+    			break;
+    		}
+    	}
+    	System.out.print(min);
+    }
+    
+    static void backtracking(int cnt, int cur, int sum, int N, int[] nums) {
+    	if (cur == N) {
+    		if (cnt > 0) {
+    			sums.add(sum);
+    		}
+    		return;
+    	}
+    	
+    	// 이번거 선택
+    	backtracking(cnt+1, cur+1, sum+nums[cur], N, nums);
+    	// 이번거 선택 안함
+    	backtracking(cnt, cur+1, sum, N, nums);
+    }
+    
+};
