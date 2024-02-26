86d51de4d541e3ce3f1d87e4dea0c65e15634c62
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-02-26 22:07:20 +0900

    Update 64064.java

--- Programmers/Lv.3/64064.java
+++ Programmers/Lv.3/64064.java
@@ -57,3 +57,58 @@ class Solution {
         }
     }
 }
+
+
+// 비트마스킹 이용
+import java.util.*;
+
+class Solution {
+    
+    static boolean[] visited;
+    static String[] selected;
+    static Set<Integer> answerSet;
+    
+    public int solution(String[] user_id, String[] banned_id) {
+        visited = new boolean[user_id.length];
+        selected = new String[banned_id.length];
+        answerSet = new HashSet<>(new HashSet<>());
+        
+        recur(0, 0, 0, user_id, banned_id);
+        
+        return answerSet.size();
+    }
+    
+    static boolean check(String user_id, String banned_id) {
+        if (user_id.length() != banned_id.length()) {
+            return false;
+        } else {
+            for (int i=0; i<user_id.length(); i++) {
+                if (banned_id.charAt(i) == '*') {
+                    continue;
+                } else {
+                    if (user_id.charAt(i) != banned_id.charAt(i)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    
+    // cur : banned_id의 인덱스
+    static void recur(int cur, int cnt, int selectedBit, String[] user_id, String[] banned_id) {
+        
+        if (cnt == banned_id.length) {
+            answerSet.add(selectedBit);
+            return;
+        }
+        
+        if (cur >= banned_id.length) return;
+        
+        for (int i=0; i<user_id.length; i++) {
+            if ((selectedBit & (1 << i)) == 0 && check(user_id[i], banned_id[cur])) {
+                recur(cur+1, cnt+1, selectedBit | (1 << i), user_id, banned_id);
+            }
+        }
+    }
+}
