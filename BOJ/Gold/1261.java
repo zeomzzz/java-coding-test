8215c75b73bb3990cda3dedba115144095b9e3ca
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-03-22 15:49:33 +0900

    Update 1261.java

--- BOJ/Gold/1261.java
+++ BOJ/Gold/1261.java
@@ -74,3 +74,79 @@ public class Main {
     }
     
 };
+
+// 다익
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, 1, -1};
+	static char[][] room;
+	static int[][] visited;
+	static PriorityQueue<Node> q;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	M = Integer.parseInt(st.nextToken());
+    	N = Integer.parseInt(st.nextToken());
+    	room = new char[N][M];
+    	
+    	String tmp;
+    	for (int r=0; r<N; r++) {
+    		tmp = br.readLine();
+    		for (int c=0; c<M; c++) room[r][c] = tmp.charAt(c);
+    	}
+    	
+    	visited = new int[N][M];
+    	for (int r=0; r<N; r++) Arrays.fill(visited[r], -1);
+    	q = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
+    	q.offer(new Node(0, 0, 0));
+    	visited[0][0] = 0;
+    	
+    	Node cur;
+    	int curR, curC, curW, nxtR, nxtC;
+    	while (!q.isEmpty()) {
+    		cur = q.poll();
+    		curR = cur.r;
+    		curC = cur.c;
+    		curW = cur.w;
+    		
+    		for (int i=0; i<4; i++) {
+    			nxtR = curR + dr[i];
+    			nxtC = curC + dc[i];
+    			
+    			if (nxtR < 0 || nxtR >= N) continue;
+    			if (nxtC < 0 || nxtC >= M) continue;
+    			if (visited[nxtR][nxtC] != -1) continue;
+    			
+    			if (room[nxtR][nxtC] == '1') {
+    				q.offer(new Node(nxtR, nxtC, curW + 1));
+    				visited[nxtR][nxtC] = curW + 1;
+    			} else {
+    				q.offer(new Node(nxtR, nxtC, curW));
+    				visited[nxtR][nxtC] = curW;
+    			}
+    			
+    		}
+    	}
+    	
+    	System.out.print(visited[N-1][M-1]);
+    }
+    
+    static class Node {
+    	int r, c, w;
+    	
+    	Node(int r, int c, int w) {
+    		this.r = r;
+    		this.c = c;
+    		this.w = w;
+    	}
+    }
+    
+};
