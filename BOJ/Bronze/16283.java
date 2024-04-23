cd015da786f7c3afcb018890a2ea2968e97e87ac
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-04-23 22:25:09 +0900

    Update 16283.java

--- BOJ/Bronze/16283.java
+++ BOJ/Bronze/16283.java
@@ -80,3 +80,49 @@ public class Main {
     }
 
 }
+
+// 240423
+import java.io.*;
+import java.util.*;
+
+// sheep + lamb = n
+// sheep * a + lamb * b == w
+
+public class Main {
+	
+	static int a, b, n, w;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	a = Integer.parseInt(st.nextToken()); // 1 ~ 1,000
+    	b = Integer.parseInt(st.nextToken()); // 1 ~ 1,000
+    	n = Integer.parseInt(st.nextToken()); // 2 ~ 1,000 -> 1 <= sheep, lamb <= 999
+    	w = Integer.parseInt(st.nextToken()); // 2 ~ 1,000,000
+    	
+    	// 완탐하면
+    	// 1000 * 1000 = 1,000,000 <= 100,000,000
+    	int cnt = 0;
+    	int sheep = 0;
+    	int lamb = 0;
+    	for (int s=1; s<=1000; s++) {
+    		for (int l=1; l<=1000; l++) {
+    			if (s + l != n) continue;
+    			if (s*a + l*b != w) continue;
+    			sheep = s;
+    			lamb = l;
+    			cnt++;
+    		}
+    	}
+    	
+    	if (cnt != 1) {
+    		System.out.print(-1);
+    	} else {
+    		System.out.print(sheep + " " + lamb);
+    	}
+    	
+    }
+    
+};
