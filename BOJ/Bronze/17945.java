20ec505bec9ea1e8067cc201a693783e0c67e851
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-04-23 22:07:58 +0900

    Update 17945.java

--- BOJ/Bronze/17945.java
+++ BOJ/Bronze/17945.java
@@ -57,3 +57,29 @@ public class Main {
     }
     
 };
+
+// 240423
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int A, B;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	A = Integer.parseInt(st.nextToken());
+    	B = Integer.parseInt(st.nextToken());
+    	
+    	for (int i=-1000; i<=1000; i++) {
+    		if (i*i + 2*A*i + B == 0) {
+    			System.out.print(i + " ");
+    		}
+    	}
+    	
+    }
+    
+};
