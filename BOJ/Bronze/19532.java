ff26177d3d23220287a5c95a2fabcb27b848ca78
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-04-25 21:37:21 +0900

    Update 19532.java

--- BOJ/Bronze/19532.java
+++ BOJ/Bronze/19532.java
@@ -28,3 +28,43 @@ public class Main {
     }
 
 };
+
+// 240425
+import java.io.*;
+import java.util.*;
+
+// 100 * 100 * 100 = 1,000,000 < 100,000,000
+
+public class Main {
+	
+	static int a, b, c, d, e, f;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	a = Integer.parseInt(st.nextToken());
+    	b = Integer.parseInt(st.nextToken());
+    	c = Integer.parseInt(st.nextToken());
+    	d = Integer.parseInt(st.nextToken());
+    	e = Integer.parseInt(st.nextToken());
+    	f = Integer.parseInt(st.nextToken());
+    	
+    	int x = 0;
+    	int y = 0;
+    	
+    	for (int i=-999; i<=999; i++) {
+    		for (int j=-999; j<=999; j++) {
+    			if (a*i + b*j != c) continue;
+    			if (d*i + e*j != f) continue;
+    			x = i;
+    			y = j;
+    		}
+    	}
+    	
+    	System.out.print(x + " " + y);
+    	
+    }
+    
+};
