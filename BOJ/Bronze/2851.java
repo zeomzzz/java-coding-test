2f0003c3af79a8b015cf6c7dc659a7ea372129d6
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-04-25 21:12:56 +0900

    Update 2851.java

--- BOJ/Bronze/2851.java
+++ BOJ/Bronze/2851.java
@@ -23,3 +23,29 @@ public class Main {
     }
 
 }
+
+// 240425
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	int sum = 0;
+    	int gap = 100;
+    	int ans = 0;
+    	for (int i=0; i<10; i++) {
+    		sum += Integer.parseInt(br.readLine());
+    		if (Math.abs(sum - 100) > gap) continue;
+    		gap = Math.abs(sum - 100);
+    		ans = Math.max(ans, sum);
+    	}
+    	
+    	System.out.print(ans);
+    }
+    
+};
