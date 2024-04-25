c17e5267f4e4b345e95900764edb75c3e5dc6e53
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-04-25 20:49:16 +0900

    Update 2875.java

--- BOJ/Bronze/2875.java
+++ BOJ/Bronze/2875.java
@@ -26,3 +26,34 @@ public class Main {
     }
 
 }
+
+// 240425
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M, K;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	K = Integer.parseInt(st.nextToken());
+    	
+    	int ans = 0;
+    	
+    	for (int n=0; n<=N; n++) { // 100
+    		for (int m=0; m<=M; m++) { // 100
+    			if (N+M-n-m != K) continue;
+    			ans = Math.max(Math.min(n/2, m), ans);
+    		}
+    	}
+    	
+    	System.out.print(ans);
+    }
+    
+};
