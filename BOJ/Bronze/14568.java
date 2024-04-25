d7635be5fb50df674e19ba7e754f41286a482ec0
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-04-25 20:25:07 +0900

    Update 14568.java

--- BOJ/Bronze/14568.java
+++ BOJ/Bronze/14568.java
@@ -25,3 +25,38 @@ public class Main {
     }
 
 }
+
+// 240425
+import java.io.*;
+import java.util.*;
+
+// 100 * 100 * 100 = 1,000,000 < 100,000,000 
+
+public class Main {
+	
+	static int N;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(br.readLine());
+    	
+    	int ans = 0;
+    	
+    	for (int a=1; a<=N; a++) { // 택희
+    		for (int b=1; b<=N; b++) { // 영훈
+    			for (int c=1; c<=N; c++) { // 남규
+    				if (a+b+c != N) continue;
+    				if (c < b+2) continue;
+    				if (a%2 == 1) continue;
+    				ans++;
+    			}
+    		}
+    	}
+    	
+    	System.out.print(ans);
+    }
+    
+};
