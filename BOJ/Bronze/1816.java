1441ac025a7483b4a7bc4f3a7c9399c6a2f89bbf
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-04-28 16:33:26 +0900

    Update 1816.java

--- BOJ/Bronze/1816.java
+++ BOJ/Bronze/1816.java
@@ -46,3 +46,57 @@ public class Main {
     }
     
 };
+
+// 240426
+import java.io.*;
+import java.util.*;
+
+// 암호의 조건
+// - 모든 소인수가 10^6보다 큰 수
+// -> 소인수가 10^6 이하면 안됨
+// -> 10^6 이하의 소수가 인수이면 안됨
+
+public class Main {
+	
+	static int N;
+	static boolean[] isPrimes;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(br.readLine());
+    	
+    	// 10^6 이하의 소수 구하기
+    	isPrimes = new boolean[1000001];
+    	Arrays.fill(isPrimes, true);
+    	isPrimes[0] = false;
+    	isPrimes[1] = false;
+    	for (int i=2; i<=1000000; i++) {
+    		for (int j=i+i; j<=1000000; j+=i) {
+    			isPrimes[j] = false;
+    		}
+    	}
+    	
+    	long S = 0;
+    	for (int i=0; i<N; i++) {
+    		S = Long.parseLong(br.readLine());
+    		if (isProper(S)) System.out.println("YES");
+    		else System.out.println("NO");
+    	}
+    	
+    }
+    
+    static boolean isProper(long S) {
+    	for (int i=2; i<=1000000; i++) {
+    		if (isPrimes[i]) {
+    			if (S % (long) i == 0) {
+    				return false;
+    			}
+    		}
+    	}
+    	
+    	return true;
+    }
+    
+};
