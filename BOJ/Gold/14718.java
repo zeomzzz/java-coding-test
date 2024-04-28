87f568ff4bfce480a3fcc53a9248504add4ec1e8
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-04-28 18:33:14 +0900

    Update 14718.java

--- BOJ/Gold/14718.java
+++ BOJ/Gold/14718.java
@@ -57,3 +57,77 @@ public class Main {
     }
     
 };
+
+// 240428
+import java.io.*;
+import java.util.*;
+
+// 1억 = 100,000,000
+// 스탯 분배 방법
+// - 썡 완탐 : 1000000 ^ 3 -> 썡 완탐하면 안됨
+// - 적의 능력치만 리스트에 넣고 탐색 : 100 * 100 * 100
+
+public class Main {
+	
+	static int N, K;
+	static int[] powers, speeds, intellis;
+	static Soldier[] soldiers;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	K = Integer.parseInt(st.nextToken());
+    	powers = new int[N];
+    	speeds = new int[N];
+    	intellis = new int[N];
+    	soldiers = new Soldier[N];
+    	
+    	for (int i=0; i<N; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		soldiers[i] = new Soldier(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+    		powers[i] = soldiers[i].power;
+    		speeds[i] = soldiers[i].speed;
+    		intellis[i] = soldiers[i].intelli;
+    	}
+    	
+    	int answer = Integer.MAX_VALUE;
+    	int cnt;
+    	for (int power : powers) {
+    		for (int speed : speeds) {
+    			for (int intelli : intellis) {
+    				if (power + speed + intelli >= answer) continue;
+    				
+    				cnt = 0;
+    				for (Soldier soldier : soldiers) {
+    					if (power < soldier.power) continue;
+    					if (speed < soldier.speed) continue;
+    					if (intelli < soldier.intelli) continue;
+    					cnt++;
+    				}
+    				if (cnt >= K) answer = power + speed + intelli;
+    			}
+    		}
+    	}
+    	
+    	System.out.print(answer);
+    	
+    }
+    
+    static class Soldier {
+    	int power;
+    	int speed;
+    	int intelli;
+    	
+    	Soldier() {}
+    	
+    	Soldier(int power, int speed, int intelli) {
+    		this.power = power;
+    		this.speed = speed;
+    		this.intelli = intelli;
+    	}
+    }
+    
+};
