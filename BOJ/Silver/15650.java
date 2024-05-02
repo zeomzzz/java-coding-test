9956cb1b8f09edc437a13f732c10dbc1ef1ab25d
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-05-02 21:25:14 +0900

    Update 15650.java

--- BOJ/Silver/15650.java
+++ BOJ/Silver/15650.java
@@ -34,3 +34,45 @@ public class Main {
     }
     
 };
+
+// 240502
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M;
+	static int[] selected;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	
+    	selected = new int[M];
+    	
+    	recur(0, 1);
+    	
+    }
+    
+    public static void recur(int cur, int start) {
+    	
+    	if (cur == M) {
+    		for (int select : selected) {
+    			System.out.print(select + " ");
+    		}
+    		System.out.println();
+    		return;
+    	}
+    	
+    	for (int i=start; i<=N; i++) {
+    		selected[cur] = i;
+    		recur(cur+1, i+1);
+    	}
+    	
+    }
+    
+};
