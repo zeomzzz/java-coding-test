b3cc0220877b09c3e6e3d70ff85bf9a9fb4b6000
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-05-02 21:18:56 +0900

    Update 15649.java

--- BOJ/Silver/15649.java
+++ BOJ/Silver/15649.java
@@ -37,3 +37,50 @@ public class Main {
     	}
     }
 };
+
+// 240502
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M;
+	static boolean[] visited;
+	static int[] selected;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	
+    	selected = new int[M];
+    	visited = new boolean[N+1];
+    	
+    	recur(0);
+    }
+    
+    public static void recur(int cur) {
+    	
+    	if (cur == M) {
+    		for (int select : selected) {
+    			System.out.print(select + " ");
+    		}
+    		System.out.println();
+    		return;
+    	}
+    	
+    	for (int i=1; i<=N; i++) {
+    		if (visited[i]) continue;
+    		
+    		selected[cur] = i;
+    		visited[i] = true;
+    		recur(cur+1);
+    		
+    		visited[i] = false;
+    	}
+    } 
+    
+};
