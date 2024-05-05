46c092d61745b27024dbe884169853fd3ca63050
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-05-05 15:53:03 +0900

    Update 15655.java

--- BOJ/Silver/15655.java
+++ BOJ/Silver/15655.java
@@ -46,3 +46,57 @@ public class Main {
     }
     
 };
+
+// 240505
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M;
+	static int[] selected, nums;
+	static StringBuilder answer;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	
+    	selected = new int[M];
+    	answer = new StringBuilder();
+    	
+    	st = new StringTokenizer(br.readLine());
+    	nums = new int[N];
+    	for (int i=0; i<N; i++) {
+    		nums[i] = Integer.parseInt(st.nextToken());
+    	}
+    	Arrays.sort(nums);
+    	
+    	recur(0, 0);
+    	
+    	System.out.print(answer);
+    }
+    
+    public static void recur(int cur, int start) {
+    	
+    	if (cur == M) {
+    		StringBuilder sb = new StringBuilder();
+    		for (int select : selected) {
+    			sb.append(select + " ");
+    		}
+    		answer.append(sb);
+    		answer.append("\n");
+    		return;
+    	}
+    	
+    	for (int i=start; i<N; i++) {
+    		selected[cur] = nums[i];
+    		recur(cur+1, i+1);
+    	}
+    	
+    }
+    
+};
