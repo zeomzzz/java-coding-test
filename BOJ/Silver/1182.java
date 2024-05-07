c3601f2bfd36c5ad5f11bc6c1a6932bcdccdcc82
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-05-07 22:26:14 +0900

    Update 1182.java

--- BOJ/Silver/1182.java
+++ BOJ/Silver/1182.java
@@ -174,3 +174,51 @@ public class Main {
     }
     
 };
+
+// 240507
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, S, answer;
+	static int[] nums;
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	S = Integer.parseInt(st.nextToken());
+    	
+    	nums = new int[N];
+    	st = new StringTokenizer(br.readLine());
+    	for (int i=0; i<N; i++) {
+    		nums[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	answer = 0;
+    	
+    	recur(0, 0, 0);
+    	
+    	System.out.print(answer);
+    }
+    
+    public static void recur(int cur, int sum, int cnt) {
+    	
+    	if (cur == N) {
+    		if (cnt != 0 && sum == S) {
+    			answer++;
+    		}
+    		return;
+    	}
+    	
+    	// 이번거 선택
+    	recur(cur+1, sum+nums[cur], cnt+1);
+    	// 이번거 선택 X
+    	recur(cur+1, sum, cnt);
+    	
+    }
+    
+};
