cc41af0eb1ea5b9aeada4e7c2e6ff085dd07f094
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-08-11 16:21:41 +0900

    Update 17945.java

--- BOJ/Bronze/17945.java
+++ BOJ/Bronze/17945.java
@@ -83,3 +83,56 @@ public class Main {
     }
     
 };
+
+// 240811
+// 1. 완탐 : 2000 * 2000
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+	static int A;
+	static int B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        
+        // x2 + 2Ax + B = 0;
+        int x1 = Integer.MIN_VALUE;
+        int x2 = Integer.MIN_VALUE;
+        
+        outer: for (int i=-1000; i<=1000; i++) {
+        	if (checkIsRoot(i)) {
+        		x1 = i;
+        		for (int j=i+1; j<=1000; j++) {
+        			if (checkIsRoot(j)) {
+        				x2 = j;
+        				break outer;
+        			}
+        		}
+        		x2 = i;
+        		break outer;
+        	}
+        }
+        
+        if (x1 == x2) {
+        	System.out.print(x1);
+        } else {
+        	System.out.print(x1 + " " + x2);
+        }
+        
+    }
+    
+    public static boolean checkIsRoot(int x) {
+    	if (x * x + 2 * A * x + B == 0) {
+    		return true;
+    	}
+    	return false;
+    }
+
+};
+
