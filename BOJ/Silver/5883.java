b7f30fb679586996c8abcfe0ed6d51600882c3fc
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-08-19 21:06:19 +0900

    Update 5883.java

--- BOJ/Silver/5883.java
+++ BOJ/Silver/5883.java
@@ -39,3 +39,63 @@ public class Main {
     }
     
 };
+
+// 240819
+import java.io.*;
+
+class Main {
+	
+	static int N;
+	static boolean[] visited;
+	static int[] nums;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+        int l = 0;
+        for (int i=0; i<N; i++) {
+        	nums[i] = Integer.parseInt(br.readLine());
+        	l = Math.max(l, nums[i]);
+        }
+        
+        visited = new boolean[l+1];
+        
+        int answer = -1;
+        for (int i=0; i<N; i++) { // idx i를 빼는 상황 => 1000
+        	int target = nums[i];
+        	int tmpCnt = 0;
+        	int prevIdx = -1;
+        	
+        	if (visited[target]) {
+        		continue;
+        	}
+        	visited[target] = true;
+        	
+        	for (int j=0; j<N; j++) { // => 1000 => 1,000,000
+        		if (nums[j] == target) continue;
+        		
+        		if (prevIdx == -1) {
+        			tmpCnt = 1;
+        		} else {
+        			if (nums[prevIdx] == nums[j]) {
+        				tmpCnt++;
+        			} else {
+        				tmpCnt = 1;
+        			}
+        			answer = Math.max(answer, tmpCnt);
+        		}
+        		
+        		prevIdx = j;
+        	}
+        }
+        
+        System.out.print(answer);
+        
+    }
+    
+};
+
+
