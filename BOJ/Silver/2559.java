cfa961b3cc05dafbbfe32240b93cd754914e67d4
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-08-22 20:34:07 +0900

    Update 2559.java

--- BOJ/Silver/2559.java
+++ BOJ/Silver/2559.java
@@ -32,3 +32,36 @@ public class Main {
     }
     
 };
+
+// 240822
+import java.util.*;
+import java.io.*;
+
+class Main {
+	
+	static int N, K;
+	static int[] prefix_sum;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+    	K = Integer.parseInt(st.nextToken());
+    	prefix_sum = new int[N+1];
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for (int i=1; i<=N; i++) {
+    		prefix_sum[i] = prefix_sum[i-1] + Integer.parseInt(st.nextToken());
+    	}
+    	
+    	int answer = Integer.MIN_VALUE;
+    	for (int i=K; i<=N; i++) {
+    		answer = Math.max(answer, prefix_sum[i] - prefix_sum[i-K]);
+    	}
+    	
+    	System.out.print(answer);
+    	
+    }
+    
+};
