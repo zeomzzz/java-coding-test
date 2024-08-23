da499a3add9bd183ec924e9637cea634ef03a781
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-08-23 23:51:20 +0900

    Update 11660.java

--- BOJ/Silver/11660.java
+++ BOJ/Silver/11660.java
@@ -43,3 +43,43 @@ public class Main {
     }
     
 };
+
+// 240823
+import java.util.*;
+import java.io.*;
+
+class Main {
+	
+	static int N, M;
+	static int[][] prefixSum;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        prefixSum = new int[N+1][N+1];
+        
+        for (int i=1; i<=N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	
+        	for (int j=1; j<=N; j++) {
+        		prefixSum[i][j] += Integer.parseInt(st.nextToken()) + prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1];
+        	}
+        }
+        
+        int x1, y1, x2, y2;
+        for (int i=0; i<M; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	x1 = Integer.parseInt(st.nextToken());
+        	y1 = Integer.parseInt(st.nextToken());
+        	x2 = Integer.parseInt(st.nextToken());
+        	y2 = Integer.parseInt(st.nextToken());
+        	
+        	System.out.println(prefixSum[x2][y2] - (prefixSum[x2][y1-1] + prefixSum[x1-1][y2]) + prefixSum[x1-1][y1-1]);
+        }
+        
+    }
+    
+};
