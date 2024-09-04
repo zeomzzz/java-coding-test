66271589d97c045e327feb3f6598725edd90a39a
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-09-04 21:36:29 +0900

    Update 10815.java

--- BOJ/Silver/10815.java
+++ BOJ/Silver/10815.java
@@ -53,3 +53,67 @@ public class Main {
     	System.out.print(sb);
     }    
 };
+
+// 240904
+/*
+ * 1. 완탐
+ * - N * M = 500,000 * 500,000 = 250,000,000,000
+ * 
+ * 2. 이분탐색
+ */
+import java.util.*;
+import java.io.*;
+
+class Main {
+	
+	static int N, M;
+	static int[] cards;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        cards = new int[N];
+        for (int i=0; i<N; i++) {
+        	cards[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(cards);
+        
+        M = Integer.parseInt(br.readLine());
+        
+        st = new StringTokenizer(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<M; i++) {
+        	int startIdx = 0;
+        	int lastIdx = N-1;
+        	int target = Integer.parseInt(st.nextToken());
+        	
+        	boolean isAnswer = false;
+        	while (!isAnswer && startIdx <= lastIdx) {
+        		
+        		int midIdx = (startIdx + lastIdx)/2;
+        		
+        		if (target == cards[midIdx]) {
+        			sb.append("1 ");
+        			isAnswer = true;
+        		} else if (target < cards[midIdx]) {
+        			lastIdx = midIdx - 1;
+        		} else {
+        			startIdx = midIdx + 1;
+        		}
+        	}
+        	
+        	if (!isAnswer) {
+        		sb.append("0 ");
+        	}
+        }
+        
+        System.out.print(sb);
+        
+    }
+   
+};
