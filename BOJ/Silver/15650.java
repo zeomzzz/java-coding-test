cfac3642390f0343535cb76590627e63001a331f
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-09-14 13:56:53 +0900

    Update 15650.java

--- BOJ/Silver/15650.java
+++ BOJ/Silver/15650.java
@@ -76,3 +76,46 @@ public class Main {
     }
     
 };
+
+// 240914
+import java.util.*;
+import java.io.*;
+
+class Main {
+	
+	static int N, M;
+	static int[] selected;
+	static StringBuilder sb;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M];
+        
+        recur(0, 1);
+        
+        System.out.print(sb);
+    }
+    
+    public static void recur(int cur, int start) {
+    	
+    	if (cur == M) {
+    		for (int select : selected) {
+    			sb.append(select + " ");
+    		}
+    		sb.append("\n");
+    		return;
+    	}
+    	
+    	for (int i=start; i<=N; i++) {
+    		selected[cur] = i;
+    		recur(cur+1, i+1);
+    	}
+    	
+    }    
+    
+};
