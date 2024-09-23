2fcb4f52ce6a9477192a65d45a3cf1c1fbd45e24
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-09-23 23:44:36 +0900

    Create 120813.java

---
+++ Programmers/Lv.0/120813.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(int) ((n+1) / 2)];
+        int idx = 0;
+        for (int i=1; i<=n; i+=2) {
+            answer[idx] = i;
+        }
+        
+        return answer;
+    }
+}
