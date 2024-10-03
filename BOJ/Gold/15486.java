21409762e82081f32054d141e357ed9f9475c912
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-10-03 22:40:40 +0900

    Update 15486.java

--- BOJ/Gold/15486.java
+++ BOJ/Gold/15486.java
@@ -40,3 +40,42 @@ public class Main {
 //    }
     
 };
+
+// 241003
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N;
+	static int[] dp;
+	static int[][] works;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        works = new int[N][2];
+        
+        for (int i=0; i<N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	works[i][0] = Integer.parseInt(st.nextToken());
+        	works[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        dp = new int[N+50];
+        Arrays.fill(dp, 0);
+        
+        for (int i=N-1; i>=0; i--) {
+        	if (i+works[i][0] <= N) { // 다음 일 받을 수 있는 날이 i+works[i][0]
+        		dp[i] = Math.max(dp[i+works[i][0]]+works[i][1], dp[i+1]);
+        	} else {
+        		dp[i] = dp[i+1]; // 다음 일 받을 수 있는 날이 i+1
+        	}
+        }
+        
+        System.out.print(dp[0]);
+    }
+
+}
