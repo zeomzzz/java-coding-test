d30208382ca041bad1ecfc2d796435aa0a14f60c
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-11-02 16:39:04 +0900

    Update 42577.java

--- Programmers/Lv.2/42577.java
+++ Programmers/Lv.2/42577.java
@@ -104,3 +104,24 @@ class Solution {
         return answer;
     }
 }
+
+// 241102
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);
+        int n = phone_book.length;
+        
+        for (int i=0; i<n-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
