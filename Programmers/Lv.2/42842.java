55bde8dacf610e32183c4dec4c84b690f6ef99e6
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2024-11-07 22:10:31 +0900

    Update 42842.java

--- Programmers/Lv.2/42842.java
+++ Programmers/Lv.2/42842.java
@@ -17,3 +17,31 @@ class Solution {
         return answer;
     }
 }
+
+// 241107
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int w = 0;
+        int h = 0;
+        outer : for (int i=1; i<2500; i++) { // 가로
+            for (int j=1; j<=i; j++) { // 세로
+                if (2*i + 2*j - 4 == brown) {
+                    if ((i-2) * (j-2) == yellow) {
+                        w = i;
+                        h = j;
+                        break outer;
+                    }
+                }
+            }
+        }
+        
+        answer[0] = w;
+        answer[1] = h;
+        
+        return answer;
+    }
+}
