7a85bcdeae512a5f03e821a61b5e6e6cf2b0ab0a
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2025-01-13 23:36:54 +0900

    Update 1978.java

--- BOJ/Bronze/1978.java
+++ BOJ/Bronze/1978.java
@@ -41,3 +41,44 @@ public class Main {
     }
     
 };
+
+// 250113
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+        for (int i=0; i<N; i++) {
+        	if (isPrime(Integer.parseInt(st.nextToken()))) {
+        		cnt++;
+        	}
+        }
+        
+        System.out.println(cnt);
+    }
+    
+    public static boolean isPrime(int n) {
+    	if (n == 1) {
+    		return false;
+    	}
+    	
+    	for (int i=2; i*i<=n; i++) {
+    		if (n % i == 0) {
+    			return false;
+    		}
+    	}
+    	
+    	return true;
+    }
+    
+}
+
